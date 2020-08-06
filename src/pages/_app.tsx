6e13a277746bdd5a259eda13d3eb44ef2477c5ba
Anderson Rosa <anderson.rosa@nodo.cc>
2020-08-05 23:46:35 -0300

    adding styled-components + global style + pwa skeleton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,34 @@
-import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React avançado boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512" />
+        <link rel="apple-touch-icon" href="/img/icon-192" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="A simple starter project" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// App.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default App
